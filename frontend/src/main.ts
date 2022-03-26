0a1bb77402b6e5435afac3d5cc7483cbf330ee87
barrfalk <bfalk@salussystems.com>
2022-03-25 17:31:49 -0700

    docker updates

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
-import {resolve } from 'path';
+import { join } from 'path';
 
 async function bootstrap () {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
 
-  app.useStaticAssets(resolve('./src/public'));
-  app.setBaseViewsDir(resolve('./src/views'));
+  app.useStaticAssets(join(__dirname, '.', 'public'));
+  app.setBaseViewsDir(join(__dirname, '.', 'views'));
   app.setViewEngine('hbs');
 
   await app.listen(3000);

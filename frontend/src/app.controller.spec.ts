9765ce2249f16b70378453e8da9774495f1029a0
barrfalk <bfalk@salussystems.com>
2022-03-28 18:11:05 -0700

    Code cleanup

--- frontend/src/app.controller.spec.ts
+++ frontend/src/app.controller.spec.ts
@@ -12,10 +12,4 @@ describe('AppController', () => {
     }).compile()
   })
 
-  describe('getHello', () => {
-    it('should return "Hello World!"', () => {
-      const appController = app.get<AppController>(AppController)
-      expect(appController.getHello()).toBe('Hello World!')
-    })
-  })
 })

7f3c8e64579f7f27e36ee768c3a1e0717075d158
barrfalk <bfalk@salussystems.com>
2022-03-28 18:21:59 -0700

    Cleaned up dependencies

--- frontend/src/app.module.ts
+++ frontend/src/app.module.ts
@@ -3,9 +3,8 @@ import {HttpService, HttpModule} from '@nestjs/axios'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { HttpConsumingService  } from './app.service.ttls'
-import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [ConfigModule.forRoot(),HttpModule],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [AppService, HttpConsumingService]
 })

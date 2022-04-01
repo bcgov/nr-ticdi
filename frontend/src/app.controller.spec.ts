650f2f686eccf8109a4eb146029a8aa0847db63d
barrfalk <bfalk@salussystems.com>
2022-04-01 09:08:53 -0700

    Corrected jest tests

--- frontend/src/app.controller.spec.ts
+++ frontend/src/app.controller.spec.ts
@@ -1,15 +1,23 @@
 import { Test, TestingModule } from '@nestjs/testing'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { HttpConsumingService } from './app.service.ttls'
+import { HttpModule} from '@nestjs/axios'
+
 
 describe('AppController', () => {
   let app: TestingModule
+  let httpConsumingService: HttpConsumingService
 
   beforeAll(async () => {
     app = await Test.createTestingModule({
+      imports: [HttpModule ],
       controllers: [AppController],
-      providers: [AppService]
+      providers: [AppService, HttpConsumingService]
     }).compile()
+
+  httpConsumingService = app.get<HttpConsumingService>(HttpConsumingService)
+
   })
 
   describe('getHello', () => {

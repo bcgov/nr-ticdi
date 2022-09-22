07a210f5d8d62f9410f593037dd673bada4f3634
barrfalk <bfalk@salussystems.com>
2022-09-22 11:51:37 -0700

    Update app.module.ts

--- frontend/src/app.module.ts
+++ frontend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TTLSService } from './ttls/ttls.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthenticationModule } from './authentication/authentication.module';
 import { SessionModule } from 'nestjs-session';
@@ -16,6 +17,6 @@ import { HttpModule } from '@nestjs/axios';
     }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TTLSService],
 })
 export class AppModule {}

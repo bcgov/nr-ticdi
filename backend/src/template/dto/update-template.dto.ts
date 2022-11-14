ee7150b48d809dc76c9c7b910f5fce6870542819
mgtennant <100305096+mgtennant@users.noreply.github.com>
2022-11-14 13:42:45 -0800

    remove now unused modules

--- backend/src/template/dto/update-template.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PickType } from "@nestjs/swagger";
-import { TemplateDto } from "./template.dto";
-
-export class UpdateTemplateDto extends PickType(TemplateDto, [
-  "template",
-  "document_type",
-  "document_version",
-  "updated_by",
-] as const) {}

9dc00a4831967c1424bf995142411e18d9f2379c
mgtennant <100305096+mgtennant@users.noreply.github.com>
2022-11-17 09:09:29 -0800

    protect admin routes

--- frontend/src/admin/admin.module.ts
+++ frontend/src/admin/admin.module.ts
@@ -4,9 +4,10 @@ import { HttpModule } from "@nestjs/axios";
 import { AdminGuard } from "./admin.guard";
 import { AdminService } from "./admin.service";
 import { AdminController } from "./admin.controller";
+import { AuthenticationModule } from "src/authentication/authentication.module";
 
 @Module({
-  imports: [HttpModule],
+  imports: [HttpModule, AuthenticationModule],
   providers: [AdminGuard, AdminService],
   exports: [AdminService],
   controllers: [AdminController],

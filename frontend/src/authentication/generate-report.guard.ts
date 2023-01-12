60a0ab7da33f0f24ea3381ba84f1779792be0e27
mgtennant <100305096+mgtennant@users.noreply.github.com>
2023-01-12 13:54:30 -0800

    allow client_roles to be null and still generate report

--- frontend/src/authentication/generate-report.guard.ts
+++ frontend/src/authentication/generate-report.guard.ts
@@ -13,18 +13,17 @@ export class GenerateReportGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request: Request & { session? } = context.switchToHttp().getRequest();
 
-    if (
-      request.session.data &&
-      request.session.data.activeAccount &&
-      request.session.data.activeAccount.client_roles
-    ) {
+    if (request.session.data && request.session.data.activeAccount) {
+      // if (request.session.data.activeAccount.client_roles) {
       // for (let entry of request.session.data.activeAccount.client_roles) {
       //   if (entry == "generate_documents") {
       //     return true;
       //   }
       // }
       // throw new ForbiddenException("Document generation privileges not found.");
+      // } else {
       return true;
+      // }
     }
   }
 }

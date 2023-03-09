ffdb105f7ab1bb5bbb4539d4526cd6638f150a6c
mgtennant <100305096+mgtennant@users.noreply.github.com>
2023-03-08 22:56:33 -0800

    update main to initialize db

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from "@nestjs/core";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
+import { AppService } from "./app.service";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,8 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
-
+  const appService = app.get(AppService);
+  await appService.initializeDb();
   await app.listen(3000);
 }
 bootstrap();

bfd0d98e58096038311bb83f94dbce5fa9ba2033
mgtennant <100305096+mgtennant@users.noreply.github.com>
2024-02-23 11:47:09 -0800

    route updates

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -3,27 +3,28 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./content/display/Header";
 import Footer from "./content/display/Footer";
 import Content from "./content/Content";
-import { CURRENT_REPORT_PAGES, PAGE } from "./util/constants";
+import { CURRENT_REPORTS, PAGE } from "./util/constants";
 
 const App: FC = () => {
   return (
     <Router>
       <Header idirUsername="Michael" isAdmin={true} />
       <Routes>
-        {Object.entries(PAGE).map(([key, path]) => (
-          <Route
-            key={key}
-            path={`/${path}`}
-            element={<Content page={path} />}
-          />
-        ))}
-        {Object.entries(CURRENT_REPORT_PAGES).map(([key, path]) => (
-          <Route
-            key={key}
-            path={`/${path}`}
-            element={<Content page={path} />}
-          />
-        ))}
+        {PAGE.map((item, index) =>
+          CURRENT_REPORTS.includes(item.title) ? (
+            <Route
+              key={index}
+              path={`/${item.path}/dtid/:dtid`} // should be `/${item.path}/dtid/12345` where 12345 can be any number
+              element={<Content page={item.title} />}
+            />
+          ) : (
+            <Route
+              key={index}
+              path={`/${item.path}/`}
+              element={<Content page={item.title} />}
+            />
+          )
+        )}
       </Routes>
       <Footer />
     </Router>

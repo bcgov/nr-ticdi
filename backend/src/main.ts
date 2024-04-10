19cc39f9daaeef5582d4c8e00152b3e2f0933862
mgtennant <100305096+mgtennant@users.noreply.github.com>
2024-04-10 16:42:51 -0700

    remove old init db logic

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { AppService } from './app.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,8 +14,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  const appService = app.get(AppService);
-  // await appService.initializeDb();
 
   await app.listen(process.env.ticdi_environment == 'DEVELOPMENT' ? 3001 : 3000);
 }

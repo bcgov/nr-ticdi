c035f8aa7c14359d93c9d622661a4011866515f0
mgtennant <100305096+mgtennant@users.noreply.github.com>
2024-05-01 10:41:27 -0700

    add assignment assumption report route

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,11 @@
 import { FC } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './content/display/Header';
 import Footer from './content/display/Footer';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import AppRoutes from './routes';
-//
+
 const App: FC = () => {
   return (
     <Provider store={store}>

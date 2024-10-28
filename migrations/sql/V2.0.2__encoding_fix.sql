07089a475024917066bde67f285e4d3dac8e938a
mgtennant <100305096+mgtennant@users.noreply.github.com>
2024-10-28 10:28:24 -0700

    encoding fixed

--- migrations/sql/V2.0.2__encoding_fix.sql
+++
@@ -1,5 +0,0 @@
--- Special brackets aren't making it through pg_dump, this corrects them
-UPDATE provision
-SET free_text = REPLACE(free_text, '┬½', '«');
-UPDATE provision
-SET free_text = REPLACE(free_text, '┬╗', '»');
